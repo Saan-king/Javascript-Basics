9bd4da20e932c8c26739ce1b3c1e09baa41f0ab5
Sathya Anand <86401617+Saan10@users.noreply.github.com>
2022-06-17 10:55:13 +0530

    final commit

---
+++ 9.remove-last-n-characters-from-string.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+function myFunction(a)
+{
+return a.slice(0,a.length-3);
+}
+
+// function myFunction(a) {
+//     return a.slice(0, -3);
+//  }
+
+// myFunction('abcdefg') Expected 'abcd'
+// myFunction('1234') Expected '1'
