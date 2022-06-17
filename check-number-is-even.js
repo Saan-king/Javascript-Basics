d6040b200b58e8c6bd724b2f86b0fe313fbfce0e
Sathya Anand <86401617+Saan10@users.noreply.github.com>
2022-06-17 11:04:29 +0530

    final commit

---
+++ check-number-is-even.js
@@ -0,0 +1,12 @@
+//Check if a number is even
+//if even,return true,else false
+function myFunction(a) {
+    if(a%2==0)
+       return true;
+    else
+       return false;
+ }
+
+// function myFunction(a) {
+//     return a % 2 === 0
+//   }
